7267d09133b4f367536ed65d00c1b9b97e2df0ef
Cat <2842641982@qq.com>
2019-08-23 17:52:33 +0800

    v0.1.0 Example Add: 1. Bool Extension Rule Change: 1. Main Program.cs only Print "Welcome to CLX"

---
+++ 4. Bool Extension/Program.cs
@@ -0,0 +1,17 @@
+﻿using CLX.Extensions.Helper;
+using CLX.Extensions.Raws;
+using System;
+
+namespace _4.Bool_Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool T = true, F = false;
+            T.Then(T.Display).Else(T.Display);
+            F.Then(F.Display).Else(F.Display);
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using CLX.Extensions.Generic;
 using CLX.Extensions.Helper;
-using CLX.Extensions.Raws;
-using CLX.Extensions.Collections;
-using CLX.ToolClass.Common;
-using System.Threading;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        public static void Test() => "Test".Display();
 
         public static void Main()
         {
-            bool T = true, F = false;
-            T.Then(T.Display).Else(T.Display);
-            F.Then(F.Display).Else(F.Display);
-            Test();
+            "Welcome To CLX!".Display();
             Console.ReadKey();
         }
     }
