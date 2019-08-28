95e96f0d1fd9091acbda5f49ebc61566e5379668
Cat <2842641982@qq.com>
2019-08-28 15:37:55 +0800

    v0.1.1 201908281540 Add: 1. Observer System 2. Simple Observer Factroy 3. Sample

---
+++ ConsoleApp1/Core/SubscribeSystem/Observer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLX
+{
+    public class Observer<T> : IObserver<T>
+    {
+        private Action onComplete;
+        private Action<Exception> onError;
+        private Action<T> onNext;
+        public void OnComplete()
+        {
+            onComplete();
+        }
+
+        public void OnError(Exception ex)
+        {
+            onError(ex);
+        }
+
+        public void OnNext(T value)
+        {
+            onNext(value);
+        }
+    }
+}
