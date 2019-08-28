9c2595e75058e104de6590bcbd09445e7400801d
Cat <2842641982@qq.com>
2019-08-28 15:37:55 +0800

    Add: 1. Observer System 2. Simple Observer Factroy 3. Sample

---
+++ ConsoleApp1/Core/Interface/IObserver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLX
+{
+    public interface IObserver<in T>
+    {
+        void OnComplete();
+        void OnError();
+        void OnNext(T value);
+    }
+}
---
+++ ConsoleApp1/Core/SubscribeSystem/Disposable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLX
+{
+    public static class Disposable
+    {
+        public static readonly IDisposable Empty = EmptyDisposable.Instance;
+
+        public static IDisposable Create(Action action)
+        {
+            return new AnonymousDisposable(action);
+        }
+
+        class EmptyDisposable : IDisposable
+        {
+            public static EmptyDisposable Instance=new EmptyDisposable();
+            EmptyDisposable()
+            {
+            }
+            public void Dispose()
+            {
+                
+            }
+        }
+
+        class AnonymousDisposable : IDisposable
+        {
+            bool isDisposed = false;
+            readonly Action dispose;
+
+            public AnonymousDisposable(Action dispose)
+            {
+                this.dispose = dispose;
+            }
+
+            public void Dispose()
+            {
+                if (!isDisposed)
+                {
+                    isDisposed = true;
+                    dispose();
+                }
+            }
+        }
+    }
+}
