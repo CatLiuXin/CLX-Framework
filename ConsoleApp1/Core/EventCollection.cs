de110163f051eecbf8ca82ad8c31508a8badc867
Cat <2842641982@qq.com>
2019-09-01 20:35:13 +0800

    v 0.1.2 201909012035

---
+++ ConsoleApp1/Core/EventCollection.cs
@@ -0,0 +1,68 @@
+﻿using CLX.Extensions.Generic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CLX
+{
+    public class EventCollection<T>: ICollection<T>
+    {
+        ICollection<T> collection;
+        Action<T> onAdd;
+        Action<T> onRemove;
+        Action<T> onClear;
+        public EventCollection(ICollection<T> collection,Action<T>onAdd=null,Action<T>onRemove=null,Action<T>onClear=null)
+        {
+            this.collection = collection;
+        }
+
+        public int Count => collection.Count;
+
+        public bool IsReadOnly => collection.IsReadOnly;
+
+        public void Add(T item)
+        {
+            collection.Add(item);
+            onAdd?.Invoke(item);
+        }
+
+        public void Clear()
+        {
+            if (onClear != null)
+            {
+                collection.ForEach(v => onClear(v));
+            }
+            collection.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return collection.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            collection.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+
+        public bool Remove(T item)
+        {
+            if (collection.Remove(item))
+            {
+                onRemove?.Invoke(item);
+                return true;
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+    }
+}
