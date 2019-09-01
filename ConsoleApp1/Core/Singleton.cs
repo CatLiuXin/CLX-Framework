39563ee9d8778782adbc46fb9bede9c5cca6a529
Cat <2842641982@qq.com>
2019-09-01 14:49:40 +0800

    v0.1.1 201909011448 Add: 1. Singleton class and Example Change: 1. SubscribeSystem Sample change

---
+++ ConsoleApp1/Core/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace CLX
+{
+    public class Singleton<T> where T:Singleton<T>
+    {
+        private Singleton<T> instance=null;
+        public Singleton<T> Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var flags = BindingFlags.NonPublic | BindingFlags.CreateInstance | BindingFlags.Instance;
+                    instance = typeof(Singleton<T>).InvokeMember(null, flags, null, null,null) as Singleton<T>;
+                    if (instance == null) throw new NullReferenceException("未能找到指定构造函数");
+                }
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+
+        }
+    }
+}
