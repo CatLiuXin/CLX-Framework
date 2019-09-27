7141f3ad060bf7b9522f3f0e48c9de52cc18a380
Cat <2842641982@qq.com>
2019-09-28 00:26:46 +0800

    Add: 1. MaxHeap单元测试

---
+++ CLX FrameworkTests/Core/Collections/MaxHeapTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CLX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLX.Tests
+{
+    [TestClass()]
+    public class MaxHeapTests
+    {
+        [TestMethod()]
+        public void MaxHeapTest()
+        {
+            List<int> testList = new List<int> {9,8,7,6,5,1,888,8};
+            testList.Sort((x, y) => y - x);
+            MaxHeap<int> maxHeap = new MaxHeap<int>(testList);
+            foreach (var v in testList)
+            {
+                Assert.AreEqual(v, maxHeap.RemoveDominating());
+            }
+        }
+
+        [TestMethod()]
+        public void MaxHeapTest1()
+        {
+            List<int> testList = new List<int> { 465, 1, 8, 9, 10 };
+            testList.Sort((x, y) => y - x);
+            MaxHeap<int> maxHeap = new MaxHeap<int> (testList);
+            foreach(var v in testList)
+            {
+                Assert.AreEqual(v, maxHeap.RemoveDominating());
+            }
+        }
+    }
+}
